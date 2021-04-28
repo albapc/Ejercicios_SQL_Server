1ab8ee9ec0dd7ad5f67bbc15d51258070d940826
Alba Perez Cesar <ALBA.PEREZCESAR@PLEXUS.ES>
2021-04-28 22:28:03 +0200

    Creado script para crear base de datos y tablas

---
+++ Ejercicio_1/TIENDADB.sql
@@ -0,0 +1,64 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'TIENDADB')
+  BEGIN
+    CREATE DATABASE [TIENDADB]
+  END
+
+  GO
+	USE [TIENDADB]
+  GO
+
+IF OBJECT_ID('MARCA', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.MARCA (
+		MarcaId int IDENTITY PRIMARY KEY,
+		Codigo int,
+		Descripcion VARCHAR(100)
+	);
+END
+
+IF OBJECT_ID('TIPOPRODUCTO', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.TIPOPRODUCTO (
+		TipoProductoId int IDENTITY PRIMARY KEY,
+		Codigo int,
+		Nombre VARCHAR(50)
+	);
+END
+
+IF OBJECT_ID('PRODUCTO', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.PRODUCTO (
+		ProductoId int IDENTITY PRIMARY KEY,
+		MarcaId int FOREIGN KEY REFERENCES dbo.MARCA(MarcaId),
+		TipoProductoId int FOREIGN KEY REFERENCES dbo.TIPOPRODUCTO(TipoProductoId),
+		Descripcion VARCHAR(100),
+		Talle VARCHAR(20),
+		Color VARCHAR(20),
+		Precio MONEY,
+		Stock int
+	);
+END
+
+IF OBJECT_ID('TICKET', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.TICKET (
+		TicketId int IDENTITY PRIMARY KEY,
+		Fecha datetime,
+		Subtotal MONEY,
+		Descuento MONEY,
+		Importe MONEY,
+		CantidadProductos int
+	);
+END
+
+IF OBJECT_ID('TICKETDETALLE', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.TICKETDETALLE (
+		TicketDetalleId int IDENTITY PRIMARY KEY,
+		TicketId int FOREIGN KEY REFERENCES dbo.TICKET(TicketId),
+		ProductoId int FOREIGN KEY REFERENCES dbo.PRODUCTO(ProductoId),
+		Descuento MONEY,
+		Importe MONEY,
+		CantidadProductos int
+	);
+END
