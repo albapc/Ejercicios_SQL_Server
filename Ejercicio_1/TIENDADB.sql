8b2051203b517b2faa1c7efe2838cff14c853141
Alba Perez Cesar <ALBA.PEREZCESAR@PLEXUS.ES>
2021-05-01 02:25:41 +0200

    añadido enunciado ejercicio 1

--- Ejercicio_1/TIENDADB.sql
+++ Ejercicio_1/TIENDADB.sql
@@ -1,3 +1,44 @@
+/*
+Crear BD en sql server (TIENDADB) con las siguientes tablas y relaciones:
+
+* MARCA:     (catálogo de marcas, Levis, H&M, Jack&Jones...)
+MarcaId            Identity
+Codigo             Numeric
+Descripcion        VARCHAR(100)
+
+
+* TIPOPRODUCTO:    (catálogo de tipo de productos - Sombrero, Perfume, Jeans, camiseta...)
+TipoProductoId    Identity
+Codigo            Numeric
+Nombre            VARCHAR(50)
+
+
+* PRODUCTO:     (Producto específico - Camiseta verde XL, Perfume hombre 50mm, Pantalon Denim 44)
+ProductoId        Identity
+MarcaId            Numeric    - FK Marca.MarcaId
+TipoProductoId    Numeric - Foreign key TIPOPRODUCTO.TipoProductoId
+Descripcion        VARCHAR(100)
+Talle            VARCHAR(20)
+Color            VARCHAR(20)
+Precio            money
+Stock            Numeric
+
+
+* TICKET:        (
+TicketId             Identity
+Fecha                datetime
+Subtotal            money
+Descuento            money
+Importe                money
+CantidadProductos    Numeric
+
+
+* TICKETDETALLE
+TicketDetalleId    Identity
+TicketId        Numeric - FK.TICKET.TicketId
+ProductoId        numeric - FK PRODUCTO.ProductoId
+*/
+
 IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'TIENDADB')
   BEGIN
     CREATE DATABASE [TIENDADB]
